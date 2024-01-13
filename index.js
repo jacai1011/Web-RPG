12127f4854c3381f7c50a9f7c62d9eb65c299453
James Cai <james.cai@student.unsw.edu.au>
2024-01-13 17:53:39 +0800

    finished cave funcs

--- index.js
+++ index.js
@@ -3,48 +3,134 @@ let health = 100;
 let gold = 50;
 let currentWeapon = 0;
 let slimeHealth = 15;
-
+let fbHealth = 60;
+let dragonHealth = 300;
 
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector('#button2');
 const button3 = document.querySelector('#button3');
 const text = document.querySelector('#text');
+const monsterStats = document.querySelector('#monsterStats');
 const healthText = document.querySelector('#healthText');
 const goldText = document.querySelector('#goldText');
 const xpText = document.querySelector('#xpText');
+const monsterName = document.querySelector('#monsterName');
+const monsterHealth = document.querySelector('#monsterHealth');
+const slimeAtkDmg = 10;
+const fbAtkDmg = 40;
 
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon; 
 
-/*fix fightDragon not allowing multiple clicks*/
-
 function healthUp() {
-    if (parseInt(goldText.innerText) == 0) {
+    if (gold == 0) {
         return;
     }
-    healthText.innerText = parseInt(healthText.innerText) + 10;
-    goldText.innerText = parseInt(goldText.innerText) - 10;
+    health += 10;
+    gold -= 10;
+    if (gold < 0) {
+        gold += 10;
+        return;
+    }
+    healthText.innerText = health;
+    goldText.innerText = gold;
 }
 
 function weaponUp() {
-    if (parseInt(goldText.innerText) < 30) {
+    if (gold < 30) {
         return;
     }
-    goldText.innerText = parseInt(goldText.innerText) - 30;
+    gold -= 30;
+    goldText.innerText = gold;
     currentWeapon += 1;
     console.log(currentWeapon);
 }
 
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function atkDmg() {
+    if (currentWeapon == 0) {
+        return randomIntFromInterval(1, 10);
+    }
+    if (currentWeapon == 1) {
+        return randomIntFromInterval(5, 15);
+    }
+    if (currentWeapon == 2) {
+        return randomIntFromInterval(10, 20);
+    }
+}
+
+function goKillScreen() {
+    monsterStats.style.display="none";
+    button1.innerText = "Go to Town Square";
+    button2.innerText = "Go to Town Square";
+    button3.innerText = "Go to Town Square";
+    text.innerText = "The monster screams \"Arg!\" as it dies. You gain experience points and find gold."
+    xpText.innerText = xp;
+    goldText.innerText = gold;
+    button1.onclick = goTown;
+    button2.onclick = goTown;
+    button3.onclick = goTown;
+}
+
+function goDeathScreen() {
+    monsterStats.style.display="none";
+    text.innerText = "You Die."
+    currentWeapon = 0;
+    health = 100;
+    gold = 50;
+    xp = 0;
+    slimeHealth = 15;
+    fbHealth = 60;
+    button1.innerText = "REPLAY?";
+    button2.innerText = "REPLAY?";
+    button3.innerText = "REPLAY?";
+    button1.onclick = goTown;
+    button2.onclick = goTown;
+    button3.onclick = goTown;
+}
+
+function atkMon(monDmg, monHealth) {
+    var damage = atkDmg();
+    if (damage >= monHealth) {
+        xp += 2;
+        gold += 13;
+        slimeHealth = 15;
+        fbHealth = 60;
+        goKillScreen();
+    }
+    else {
+        health -= monDmg;
+        healthText.innerText = health;
+        if (health <= 0) {
+            goDeathScreen();
+        }
+        monHealth -= damage;
+        monsterHealth.innerText = monHealth;
+    }
+    slimeHealth = monHealth;
+    fbHealth = monHealth;
+}
+
+function dodge() {
+    text.innerText = "You dodge the attack.";
+}
+
 function goTown() {
-    document.getElementById("monsterStats").style.display="none";
+    monsterStats.style.display="none";
     button1.innerText = "Go to Store";
     button2.innerText = "Go to Cave";
     button3.innerText = "Fight Dragon";
-    text.innerText = "You are in the town sqaure. You see the sign that says \"Store\""
+    text.innerText = "You are in the town square. You see the sign that says \"Store\""
     button1.onclick = goStore;
     button2.onclick = goCave;
     button3.onclick = fightDragon;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+    xpText.innerText = xp;
 }
 
 function goStore() {
@@ -63,6 +149,7 @@ function goCave() {
     button3.innerText = "Go to town square";
     text.innerText = "You enter the cave. You see some monsters.";
     button1.onclick = fightSlime;
+    button2.onclick = fightFb;
     button3.onclick = goTown;
 }
 
@@ -72,7 +159,9 @@ function fightDragon() {
     button3.innerText = "Run";
     text.innerText = "You are fighting a monster.";
     button3.onclick = goTown;
-    document.getElementById("monsterStats").style.display="block";
+    monsterStats.style.display="block";
+    monsterHealth.innerText = dragonHealth;
+    monsterName.innerText = 'Dragon';
 }
 
 function fightSlime() {
@@ -80,6 +169,27 @@ function fightSlime() {
     button2.innerText = "Dodge";
     button3.innerText = "Run";
     text.innerText = "You are fighting a monster.";
+    button1.onclick = function() {
+        atkMon(slimeAtkDmg, slimeHealth);
+    };
+    button2.onclick = dodge;
     button3.onclick = goTown;
-    document.getElementById("monsterStats").style.display="block";
+    monsterStats.style.display="block";
+    monsterHealth.innerText = slimeHealth;
+    monsterName.innerText = 'Slime';
+}
+
+function fightFb() {
+    button1.innerText = "Attack";
+    button2.innerText = "Dodge";
+    button3.innerText = "Run";
+    text.innerText = "You are fighting a monster.";
+    button1.onclick = function() {
+        atkMon(fbAtkDmg, fbHealth);
+    };
+    button2.onclick = dodge;
+    button3.onclick = goTown;
+    monsterStats.style.display="block";
+    monsterHealth.innerText = fbHealth;
+    monsterName.innerText = 'Fanged Beast';
 }
--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@ An interactive webpage styled as a RPG built in HTML, CSS and JS.
 ### Target Goals
 Further develop HTML, CSS understanding whilst developing JS fundamentals.
 ### Objectives Achieved
+Learned HTML tips and tricks along with revising JS fundamentals. Future projects should focus on DOM and JS compartmentalising.
